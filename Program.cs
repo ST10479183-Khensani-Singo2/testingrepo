241fe55bda962eef04ec82b06237896ec6e46ec9
ST10479183-Khensani-Singo2 <st10479183@myemeris.edu.za>
2026-08-14 10:35:24 +0200

    Commiting my code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TutoringLesson
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a C# console application");
+            Console.WriteLine("Running an NET 10.0 ");
+        }
+    }
+}
